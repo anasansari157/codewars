2020d0b823ac9eed0539b15c7296cac2a0edc780
Anas Ansari <anasansari157@gmail.com>
2020-03-30 19:02:41 +0530

    fetching user

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-console.log('JS Connected');
+console.log("JS Connected");
 
+let usernameInput = document.body.getElementsByClassName("username-input");
 
-let usernameInput = document.body.getElementsByClassName('username-input');
-console.log(usernameInput);
+function getUser() {
+	event.preventDefault();
+	console.log(event, "event");
+
+	let codewarsUserUrl = `https://www.codewars.com/api/v1/users/bim240`;
+	console.log(codewarsUserUrl);
+
+	//fetch
+	fetch(codewarsUserUrl, {
+    mode: "no-cors"
+	})
+		.then((response) => {
+			console.log("response", response);
+			return response.json();
+		})
+		.then((jsonData) => {
+			console.log(jsonData);
+			return jsonData;
+		});
+}
